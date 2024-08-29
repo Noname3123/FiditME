82163560e73cc2f3cd876dffd74151cd39354c11
Benjamin Jakupovic <bjakupo@gmail.com>
2024-08-29 12:03:10 +0200

    updated role settings to match the final app

--- fidit_me_jakupovic/lib/models/definedroles.dart
+++ fidit_me_jakupovic/lib/models/definedroles.dart
@@ -2,9 +2,36 @@ import 'package:fidit_me_jakupovic/models/role_settings.dart';
 
 ///A Map of roles and corresponding classes which define what is visible to the user. Key is the name of the role while the value is the appropriate role setting
 final definedRoleMap = {
-  "Student": RoleSettings(canSeeDinp: false),
-  "Undergraduate": RoleSettings(canSeeDinp: false),
-  "Graduate": RoleSettings(canSeeDinp: false)
+  "Student": RoleSettings(
+    hideDinpPage: true,
+    hideGraduateStudiesEnrollment: true,
+    hideGraduateStudyDINPS: true,
+    hideUnderGraduateStudyDINPS: true,
+    hideSeniorYearsEnrollmentDocs: true,
+    hideUndergraduateStudiesEnrollment: false,
+    hidePlanOfGraduateStudies: false,
+    hidePlanOfUndergraduateStudies: false,
+  ),
+  "Undergraduate": RoleSettings(
+    hideDinpPage: false,
+    hideGraduateStudiesEnrollment: false,
+    hideGraduateStudyDINPS: true,
+    hideUnderGraduateStudyDINPS: false,
+    hideSeniorYearsEnrollmentDocs: false,
+    hideUndergraduateStudiesEnrollment: true,
+    hidePlanOfGraduateStudies: false,
+    hidePlanOfUndergraduateStudies: false,
+  ),
+  "Graduate": RoleSettings(
+    hideDinpPage: false,
+    hideGraduateStudiesEnrollment: true,
+    hideGraduateStudyDINPS: false,
+    hideUnderGraduateStudyDINPS: true,
+    hideSeniorYearsEnrollmentDocs: false,
+    hideUndergraduateStudiesEnrollment: true,
+    hidePlanOfGraduateStudies: false,
+    hidePlanOfUndergraduateStudies: true,
+  )
 };
 
 ///For a given RoleSetting, find the appropriate key in the definedRoleMap or return the default key (String is the return type)
--- fidit_me_jakupovic/lib/models/role_settings.dart
+++ fidit_me_jakupovic/lib/models/role_settings.dart
@@ -1,7 +1,36 @@
 ///this is a class of bool fields which define which parts of the app are available to user. Each role has appropriate RoleSettings associated with it
 class RoleSettings {
-  RoleSettings(
-      {required this.canSeeDinp}); //TODO: create class which will be used for configuring app from role perspective
+  RoleSettings({
+    required this.hideDinpPage,
+    required this.hidePlanOfUndergraduateStudies,
+    required this.hidePlanOfGraduateStudies,
+    required this.hideUndergraduateStudiesEnrollment,
+    required this.hideGraduateStudiesEnrollment,
+    required this.hideSeniorYearsEnrollmentDocs,
+    required this.hideUnderGraduateStudyDINPS,
+    required this.hideGraduateStudyDINPS,
+  }); //TODO: create class which will be used for configuring app from role perspective
+  ///Should hide dinp page from user?
+  bool hideDinpPage = false;
 
-  bool canSeeDinp = false;
+  ///Is the undergraduate plan hidden
+  bool hidePlanOfUndergraduateStudies = false;
+
+  ///Is the graduate plan hidden from person
+  bool hidePlanOfGraduateStudies = false;
+
+  ///Should the docs instructing on how to enroll to the 1st year of undergraduate study be hidden
+  bool hideUndergraduateStudiesEnrollment = false;
+
+  ///Should the docs instructing on how to enroll to the 1st year of graduate study be hidden
+  bool hideGraduateStudiesEnrollment = false;
+
+  ///Should the docs instructing on how to enroll to senior years be hidden
+  bool hideSeniorYearsEnrollmentDocs = false;
+
+  ///Should the DINP docs of the graduate studies be hidden
+  bool hideGraduateStudyDINPS = false;
+
+  ///Should the DINP docs of the undergraduate studies be hidden
+  bool hideUnderGraduateStudyDINPS = false;
 }
