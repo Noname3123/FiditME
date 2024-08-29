c35b894a223dd8f356442469d51104219eb18b59
Benjamin Jakupovic <bjakupo@gmail.com>
2024-08-29 10:15:46 +0200

    Implemented all internet documents and appropriate localizations

--- fidit_me_jakupovic/lib/models/internet_documents.dart
+++ fidit_me_jakupovic/lib/models/internet_documents.dart
@@ -64,3 +64,71 @@ var dinpDiplomskiStudiji = {
     )
   ]
 };
+
+///This var is of type Map<"String", List<Document>>
+var dokumentiUpisDiplomskiStudij = {
+  "DiplomskiStudij": [
+    Document(
+      url: Uri(
+          scheme: "https",
+          host: "inf.uniri.hr",
+          path:
+              "/images/studiji/upisi/2024/Natjecaj_upisi_diplomski_studiji_2024_2025.pdf"),
+      documentName: "NatjecajZaUpisUPrvuGodinuDiplomskog",
+    )
+  ]
+};
+
+var dokumentiUpisPredDiplomskiStudij = {
+  "PredDiplomskiStudij": [
+    Document(
+      url: Uri(
+          scheme: "https",
+          host: "inf.uniri.hr",
+          path:
+              "/images/studiji/upisi/2024/Natjecaj_upisi_prijediplomski_2024_2025.pdf"),
+      documentName: "NatjecajZaUpisUPrvuGodinuPredDiplomskog",
+    )
+  ]
+};
+
+var dokumentiUpisViseGodine = {
+  "UpisViseGodine": [
+    Document(
+      url: Uri(
+          scheme: "https",
+          host: "inf.uniri.hr",
+          path:
+              "/images/studiji/upisi/2023/Odluka_o_uvjetima_upisa_u_visu_godinu_studija.pdf"),
+      documentName: "OdlukaOUvjetimaUpisaUVisuGodinuStudija",
+    )
+  ]
+};
+
+///This Map<String, List<Document>> contains a list of all documents related to the program of the undergraduate study
+var dokumentPrijediplomskiProgram = {
+  "ProgramPredDiplomski": [
+    Document(
+      url: Uri(
+          scheme: "https",
+          host: "inf.uniri.hr",
+          path:
+              "/images/studiji/preddiplomski/1P/program/NPP_Prijediplomski_studij_Informatika_FIDIT_2022_2.pdf"),
+      documentName: "PlanProgramPredDiplomskog",
+    )
+  ]
+};
+
+///This Map<String, List<Document>> contains a list of all documents related to the program of the graduate study
+var dokumentDiplomskiProgram = {
+  "ProgramDiplomski": [
+    Document(
+      url: Uri(
+          scheme: "https",
+          host: "inf.uniri.hr",
+          path:
+              "/images/studiji/diplomski/IIS_PI/NPP_Diplomski_studij_Informatika_FIDIT_2022_2.pdf"),
+      documentName: "PlanProgramDiplomskog",
+    )
+  ]
+};
